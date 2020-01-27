c215176a806ab636b789f78a00d7dbdde3a733d2
Овдин Кирилл Валерьевич <kovdin@at-consulting.ru>
2020-01-27 17:19:01 +0300

    initial commit

---
+++ routes/auth.routes.js
@@ -0,0 +1,10 @@
+const { Router } = require('express');
+const router = Router();
+
+// api/auth/register
+router.post('/register', async (req, res) => {});
+
+// api/auth/login
+router.post('/login', async (req, res) => {});
+
+module.exports = router;
